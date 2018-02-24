67449dbaaca8a0316a746e4b320489b5e007cbed
Cursi <razvan.diib@yahoo.com>
2018-02-25 01:46:16 +0200

    Quick start files & NavBar

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent
+{
+  title = "Notiz";
+  iconPath = "./assets/icon.png";
+
+  constructor(private electronService: ElectronService) {}
+}
