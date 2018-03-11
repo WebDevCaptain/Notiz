7b0d9e260ee68bef2e9ea099129ae281d92aa9ee
Cursi <razvan.diib@yahoo.com>
2018-03-11 02:46:23 +0200

    Solved small UI problems and added specific note UI

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -10,9 +10,34 @@ export class NotesComponent implements OnInit {
 
   constructor(public appComponent: AppComponent) { }
 
+  notes : string;
+  // [
+  //   {
+  //     "title": "Note1",
+  //     "content": "Lala1",
+  //     "foregroundColor": "#111",
+  //     "backgroundColor": "#222"
+  //   },
+  //   {
+  //     "title": "Note2",
+  //     "content": "Lala2",
+  //     "foregroundColor": "#222",
+  //     "backgroundColor": "#333"
+  //   },
+  //   {
+  //     "title": "Note3",
+  //     "content": "Lala3",
+  //     "foregroundColor": "#333",
+  //     "backgroundColor": "#444"
+  //   }
+  // ];
+
+  // localStorage.setItem('notes', JSON.stringify(this.notes));
+  // localStorage.clear();
+
   ngOnInit()
   {
-
+    this.notes = localStorage.getItem('notes');
+    console.log('notes: ', JSON.parse(this.notes));
   }
-
 }
--- src/app/specific-note/specific-note.component.ts
+++ src/app/specific-note/specific-note.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-specific-note',
   templateUrl: './specific-note.component.html',
   styleUrls: ['./specific-note.component.css']
 })
-export class SpecificNoteComponent implements OnInit {
-
+export class SpecificNoteComponent implements OnInit
+{
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit()
+  {
+    $(document).ready(function()
+    {
+      // $("textarea").keyup();
+    });
   }
-
 }
